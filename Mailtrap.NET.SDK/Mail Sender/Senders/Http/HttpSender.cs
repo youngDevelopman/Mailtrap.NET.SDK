f66fa8d0bf512dd579e25864ddbfd86bc7ed1b76
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-14 20:26:13 +0300

    Implement static http client with pooled connection lifetime to avoid DNS draining issue

--- Mailtrap.NET.SDK/Mail Sender/Senders/Http/HttpSender.cs
+++ Mailtrap.NET.SDK/Mail Sender/Senders/Http/HttpSender.cs
@@ -2,30 +2,36 @@
 using Mailtrap.NET.SDK.Exceptions;
 using Mailtrap.NET.SDK.MailSender.Extensions;
 using Mailtrap.NET.SDK.Models;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Mailtrap.NET.SDK.MailSender.Senders.Http
 {
     internal class HttpSender : IMailSender
     {
+        static HttpClient httpClient;
+        static HttpSender() 
+        {
+            var socketsHandler = new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(2)
+            };
+            httpClient = new HttpClient(socketsHandler);
+        }
+
         public string Url { get; }
         private readonly HttpCredentials _credentials;
         public HttpSender(string url, HttpCredentials credentials)
         {
             Url = url ?? throw new ArgumentNullException($"{nameof(Url)} parameter cannot be null");
             _credentials = credentials ?? throw new ArgumentNullException($"Credentials parameter cannot be null");
+            AddHeaders(httpClient);
         }
 
         public async Task SendAsync(SendEmailRequest sendEmailRequest, CancellationToken cancellationToken)
         {
-            var httpClient = new HttpClient()
-            {
-                BaseAddress = new Uri(Url),
-            };
-
-            AddHeaders(httpClient);
             var mailtrapRequestModel = await sendEmailRequest.MapToHttpCompliantModelAsync();
-            var result = await httpClient.PostAsJsonAsync(string.Empty, mailtrapRequestModel, cancellationToken);
+            var result = await httpClient.PostAsJsonAsync(Url, mailtrapRequestModel, cancellationToken);
             
             if (!result.IsSuccessStatusCode)
             {
@@ -36,7 +42,7 @@ namespace Mailtrap.NET.SDK.MailSender.Senders.Http
 
         private void AddHeaders(HttpClient httpClient)
         {
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_credentials.Token}");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _credentials.Token);
         }
     }
 }
